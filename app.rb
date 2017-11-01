8dbd49598cd6176e32c6a903fa0295cae6243ee4
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-01 00:29:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     text_from_user = params[:user_text]
 
     @analyzed_text = TextAnalyzer.new(text_from_user)
-    @c = "butt"
+    @c = @analyzed_text.count_of_words
     erb :results
   end
 end
